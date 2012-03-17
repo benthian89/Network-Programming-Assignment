cbe9c48f2964a8af12dc2f8ad638e43c7a1a8cf9
benthian89 <ben_thian@hotmail.com>
2012-03-17 17:55:21 +0800

    upload source files

---
+++ src/UDPClient.java
@@ -0,0 +1,37 @@
+//
+import java.net.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class UDPClient {
+	
+	public static void main (String args[]) throws Exception {
+
+		//Use DataGramSocket for UDP connection
+		DatagramSocket s = new DatagramSocket();
+		
+		// convert string "hello" to array of bytes, suitable for
+		// creation of DatagramPacket
+		Path path = Paths.get("G:/Dropbox/Works/Eclipse/CS2105/test.txt");		
+		byte outBuf[] = Files.readAllBytes(path);
+
+		// Now create a packet (with destination addr and port)
+		InetAddress addr = InetAddress.getByName("localhost");
+		int port = 9001;
+		DatagramPacket outPkt = new DatagramPacket(outBuf, outBuf.length,
+			 addr, port);
+		s.send(outPkt);
+		
+		// create a packet buffer to store data from packets received.
+		byte inBuf[] = new byte[1000];
+		DatagramPacket inPkt = new DatagramPacket(inBuf, inBuf.length);
+
+		// receive the reply from server.
+		s.receive(inPkt);
+
+		// convert reply to string and print to System.out
+		String reply = new String(inPkt.getData(), 0, inPkt.getLength());
+		System.out.println(reply);
+	}
+}
---
+++ src/UDPServer.java
@@ -0,0 +1,46 @@
+//
+import java.net.*;
+import java.io.*;
+
+public class UDPServer {
+	
+	public static void main (String args[]) throws Exception 
+	{
+		//use DatagramSocket for UDP connection
+		DatagramSocket s = new DatagramSocket(9001);
+		byte[] inBuf = new byte[1000];			
+		
+		while (true)
+		{
+			DatagramPacket inPkt = new DatagramPacket(inBuf, inBuf.length);
+			s.receive(inPkt);
+			// convert content of packet into a string 
+			FileOutputStream myFile = new FileOutputStream("testrecieved.txt");	
+			
+			myFile.write(inPkt.getData(),inPkt.getOffset(),inPkt.getLength());				
+			myFile.close();
+			
+			
+			
+//			String request = new String(inPkt.getData(), 0, 
+	//			inPkt.getLength());
+
+			// convert string to uppercase
+			String reply = "File received!";
+			
+			// convert upper-case string into array of bytes (output
+			// buffer)
+			byte[] outBuf = new byte[1000];
+			outBuf = reply.getBytes();
+
+			// create reply packet using output buffer.
+			// Note: dest address/port is retrieved from inPkt
+			DatagramPacket outPacket = new DatagramPacket(
+				outBuf, outBuf.length, inPkt.getAddress(),
+				inPkt.getPort());
+
+			// finally, send the packet
+			s.send(outPacket);
+		}
+	}
+}
