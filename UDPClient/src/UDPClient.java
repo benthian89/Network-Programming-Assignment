9dabef52b3406e146d4b81429749dc037db7afd6
benthian89 <ben_thian@hotmail.com>
2012-03-18 17:34:23 +0800

    Added filechooser, sends filename first then the actual file. Simple check inplace for filename and acknowledgement.

--- UDPClient/src/UDPClient.java
+++ UDPClient/src/UDPClient.java
@@ -1,37 +1,81 @@
 //
 import java.net.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.*;
+import javax.swing.*;
 
 public class UDPClient {
 	
 	public static void main (String args[]) throws Exception {
-
+		
 		//Use DataGramSocket for UDP connection
 		DatagramSocket s = new DatagramSocket();
 		
-		// convert string "hello" to array of bytes, suitable for
-		// creation of DatagramPacket
-		Path path = Paths.get("Data/test.txt");		
-		byte outBuf[] = Files.readAllBytes(path);
-
-		// Now create a packet (with destination addr and port)
+		//Addr and port number created
 		InetAddress addr = InetAddress.getByName("localhost");
 		int port = 9001;
-		DatagramPacket outPkt = new DatagramPacket(outBuf, outBuf.length,
-			 addr, port);
-		s.send(outPkt);
 		
-		// create a packet buffer to store data from packets received.
-		byte inBuf[] = new byte[1000];
-		DatagramPacket inPkt = new DatagramPacket(inBuf, inBuf.length);
+		//Lets the user choose the file
+		JFileChooser chooser = new JFileChooser();
+        chooser.showOpenDialog(null);
+        File myFile = chooser.getSelectedFile();
+        String filename = myFile.getName();
+		
+        //Converts filename to byte for sending via DatagramPacket
+        byte[] filenameByte = filename.getBytes();
+       
+        //Created a packet ( with destination addr and port )
+        //Sends over the filename first
+        DatagramPacket outFilename = new DatagramPacket(filenameByte, filenameByte.length,
+        		addr, port);
+        
+        s.send(outFilename);
+        
+     // create a packet buffer to store data from packets received.
+     		byte infilenameReply[] = new byte[1000];
+     		DatagramPacket infilename = new DatagramPacket(infilenameReply, infilenameReply.length);
 
-		// receive the reply from server.
-		s.receive(inPkt);
+     		// receive the reply from server.
+     		s.receive(infilename);
 
-		// convert reply to string and print to System.out
-		String reply = new String(inPkt.getData(), 0, inPkt.getLength());
-		System.out.println(reply);
+     		// convert reply to string and print to System.out
+     		String reply = new String(infilename.getData(), 0, infilename.getLength());
+     		if (reply.equalsIgnoreCase("Filename received!")) {    			
+     			System.out.println(reply);
+     			//Convert the file into byteArray
+     			FileInputStream fis =  new FileInputStream(myFile);
+        
+     			long length = myFile.length();
+     			if (length > Integer.MAX_VALUE){
+     				System.out.println("File is too large");
+     				System.exit(0);
+     			}
+     			byte[] outBuf = new byte[(int)length]; 
+		
+     			fis.read(outBuf);
+
+		
+     			// Now create a packet (with destination addr and port)
+     			// Sends over the actual file
+     			DatagramPacket outPkt = new DatagramPacket(outBuf, outBuf.length,
+     					addr, port);
+     			s.send(outPkt);
+		
+     			// create a packet buffer to store data from packets received.
+     			byte inBuf[] = new byte[1000];
+     			DatagramPacket inPkt = new DatagramPacket(inBuf, inBuf.length);
+
+     			// receive the reply from server.
+     			s.receive(inPkt);
+
+     			// convert reply to string and print to System.out
+     			String reply2 = new String(inPkt.getData(), 0, inPkt.getLength());
+     			System.out.println(reply2);
+     		}
+     		else {
+     			System.out.println("No acknowledgement from server that filename is received");
+     			System.exit(0);
+     		}
 	}
 }
+
+
--- UDPServer/src/UDPServer.java
+++ UDPServer/src/UDPServer.java
@@ -8,30 +8,45 @@ public class UDPServer {
 	{
 		//use DatagramSocket for UDP connection
 		DatagramSocket s = new DatagramSocket(9001);
-		byte[] inBuf = new byte[1000];			
+		byte[] inBuf = new byte[1000];	
+		byte[] infilenameByte = new byte[1000];
 		
 		while (true)
 		{
+			
+			DatagramPacket infilename = new DatagramPacket(infilenameByte, infilenameByte.length);
+			s.receive(infilename);
+			
+			String filename = new String(infilename.getData());
+			if (!filename.equals(null)) {
+			
+				String reply = "Filename received!";
+			
+				byte[] outfilenameByte = new byte[1000];
+				outfilenameByte = reply.getBytes();
+
+				// create reply packet using outfilename buffer.
+				// Note: dest address/port is retrieved from infilename
+				DatagramPacket outfilename = new DatagramPacket(
+				outfilenameByte, outfilenameByte.length, infilename.getAddress(),
+				infilename.getPort());
+
+				s.send(outfilename);
+													
 			DatagramPacket inPkt = new DatagramPacket(inBuf, inBuf.length);
 			s.receive(inPkt);
-			// convert content of packet into a string 
-			FileOutputStream myFile = new FileOutputStream("Data/testrecieved.txt");	
+			// convert content of packet into a file
+			FileOutputStream myFile = new FileOutputStream("Data/"+filename);	
 			
 			myFile.write(inPkt.getData(),inPkt.getOffset(),inPkt.getLength());				
 			myFile.close();
 			
-			
-			
-//			String request = new String(inPkt.getData(), 0, 
-	//			inPkt.getLength());
 
-			// convert string to uppercase
-			String reply = "File received!";
-			
-			// convert upper-case string into array of bytes (output
-			// buffer)
+			// Create reply
+			String reply2 = "File received!";
+					
 			byte[] outBuf = new byte[1000];
-			outBuf = reply.getBytes();
+			outBuf = reply2.getBytes();
 
 			// create reply packet using output buffer.
 			// Note: dest address/port is retrieved from inPkt
@@ -41,6 +56,13 @@ public class UDPServer {
 
 			// finally, send the packet
 			s.send(outPacket);
+			
+			}
+			
+			else {
+				System.out.println("filename not received");
+			}
+			
 		}
 	}
 }
