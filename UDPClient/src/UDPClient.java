d35d7c91e925f99be4840e842fa4e3d22290f6ae
benthian89 <ben_thian@hotmail.com>
2012-03-21 19:58:19 +0800

    Now left implementation of Stop & wait for the file transfer to work.
    After that need checksum to ensure data is not corrupted.

--- UDPClient/src/UDPClient.java
+++ UDPClient/src/UDPClient.java
@@ -13,9 +13,13 @@ public class UDPClient {
 		//Addr and port number created
 		InetAddress addr = InetAddress.getByName("localhost");
 		int port = 9001;
-		
+		 try {
+	     		UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+	     	} catch (Exception e) {
+	     		System.out.println("Unable to load Windows look and feel");
+	     	}
 		//Lets the user choose the file
-		JFileChooser chooser = new JFileChooser();
+		JFileChooser chooser = new JFileChooser(".");
         chooser.showOpenDialog(null);
         File myFile = chooser.getSelectedFile();
         String filename = myFile.getName();
@@ -43,23 +47,47 @@ public class UDPClient {
      			System.out.println(reply);
      			//Convert the file into byteArray
      			FileInputStream fis =  new FileInputStream(myFile);
-        
      			long length = myFile.length();
      			if (length > Integer.MAX_VALUE){
      				System.out.println("File is too large");
      				System.exit(0);
      			}
-     			byte[] outBuf = new byte[(int)length]; 
-		
-     			fis.read(outBuf);
-
-		
+     			// total number of packets to send
+     			int total_packets = ((int)length / 65500) + 1;   					
+     			// flag for indication of last packet
+     			int last_packet = -1;    			
+     			// 1 byte is used for the last packet flag
+     			byte last_packetByte;     			
+     			int packet_number = 0;
+     			
+     	     			   			
+     			while ( total_packets > 0) {
+     				byte[] outBuf = new byte[65502];
+     				packet_number++;
+     				// packet numbering will start from 1
+     				// 1 byte is used for keeping track of packet number
+         			outBuf[65500] = (byte) packet_number;
+         				if (total_packets == 1) {
+         				last_packet = 1;
+         				last_packetByte = (byte) last_packet;
+         				outBuf[65501] = last_packetByte;
+         				}
+         				else {
+         					last_packet = 0;
+         					last_packetByte = (byte) last_packet;
+         					outBuf[65501] = last_packetByte;
+         					
+         				}
+     				fis.read(outBuf,0,(int)length%65500);
+    			
      			// Now create a packet (with destination addr and port)
      			// Sends over the actual file
-     			DatagramPacket outPkt = new DatagramPacket(outBuf, outBuf.length,
-     					addr, port);
-     			s.send(outPkt);
-		
+     				DatagramPacket outPkt = new DatagramPacket(outBuf, outBuf.length,
+     						addr, port);
+     				s.send(outPkt);
+     				System.out.println(packet_number+"");
+     				total_packets--;
+     			}
      			// create a packet buffer to store data from packets received.
      			byte inBuf[] = new byte[1000];
      			DatagramPacket inPkt = new DatagramPacket(inBuf, inBuf.length);
--- UDPServer/src/UDPServer.java
+++ UDPServer/src/UDPServer.java
@@ -8,61 +8,82 @@ public class UDPServer {
 	{
 		//use DatagramSocket for UDP connection
 		DatagramSocket s = new DatagramSocket(9001);
-		byte[] inBuf = new byte[1000];	
+		byte[] inBuf = new byte[65502];
 		byte[] infilenameByte = new byte[1000];
+		byte[] temp = new byte[65502];
+		FileOutputStream myFile;
+		String filename;
+		
+		DatagramPacket infilename = new DatagramPacket(infilenameByte, infilenameByte.length);
+		s.receive(infilename);
+		
+		 filename = new String(infilename.getData());
+		if (!filename.equals(null)) {
+		
+			String reply = "Filename received!";
+			
+			byte[] outfilenameByte = new byte[1000];
+			outfilenameByte = reply.getBytes();
+
+			// create reply packet using outfilename buffer.
+			// Note: dest address/port is retrieved from infilename
+			DatagramPacket outfilename = new DatagramPacket(
+			outfilenameByte, outfilenameByte.length, infilename.getAddress(),
+			infilename.getPort());
+				
+			s.send(outfilename);
+		}
+		else {
+			System.out.println("filename not received");
+			System.exit(0);
+		}
+		
+		myFile = new FileOutputStream("Data/"+filename);
 		
 		while (true)
 		{
 			
-			DatagramPacket infilename = new DatagramPacket(infilenameByte, infilenameByte.length);
-			s.receive(infilename);
-			
-			String filename = new String(infilename.getData());
-			if (!filename.equals(null)) {
-			
-				String reply = "Filename received!";
-			
-				byte[] outfilenameByte = new byte[1000];
-				outfilenameByte = reply.getBytes();
-
-				// create reply packet using outfilename buffer.
-				// Note: dest address/port is retrieved from infilename
-				DatagramPacket outfilename = new DatagramPacket(
-				outfilenameByte, outfilenameByte.length, infilename.getAddress(),
-				infilename.getPort());
-
-				s.send(outfilename);
-													
 			DatagramPacket inPkt = new DatagramPacket(inBuf, inBuf.length);
 			s.receive(inPkt);
 			// convert content of packet into a file
-			FileOutputStream myFile = new FileOutputStream("Data/"+filename);	
-			
-			myFile.write(inPkt.getData(),inPkt.getOffset(),inPkt.getLength());				
-			myFile.close();
-			
-
-			// Create reply
-			String reply2 = "File received!";
-					
-			byte[] outBuf = new byte[1000];
-			outBuf = reply2.getBytes();
-
-			// create reply packet using output buffer.
-			// Note: dest address/port is retrieved from inPkt
-			DatagramPacket outPacket = new DatagramPacket(
-				outBuf, outBuf.length, inPkt.getAddress(),
-				inPkt.getPort());
-
-			// finally, send the packet
-			s.send(outPacket);
-			
+			 			
+			int packet_number = 0;			
+			temp = inPkt.getData();
+				
+			// check that packet is not the last packet
+			if ((int)temp[65501] != 1) {
+				packet_number++;
+				// check that the packet_number is in sequence
+				if ((int)temp[65500] == packet_number) {
+						myFile.write(temp);
+				}
+				else {
+					//send ack for the previous packet number
+				}				
 			}
-			
-			else {
-				System.out.println("filename not received");
-			}
-			
+			// Last packet
+			else if ( (int) temp[65501] == 1) {
+				 											
+				 		myFile.write(temp);
+				 		myFile.close();
+				 					 		
+				 		// Create reply
+						String reply2 = "File received!";
+								
+						byte[] outBuf = new byte[1000];
+						outBuf = reply2.getBytes();
+
+						// create reply packet using output buffer.
+						// Note: dest address/port is retrieved from inPkt
+						DatagramPacket outPacket = new DatagramPacket(
+							outBuf, outBuf.length, inPkt.getAddress(),
+							inPkt.getPort());
+
+						// finally, send the packet
+						s.send(outPacket);
+				 						 		
+			}			
+						
 		}
 	}
 }
