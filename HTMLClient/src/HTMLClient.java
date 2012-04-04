e102d6b7690a20a865aa0d70d58feeaa0c77bb52
benthian89 <ben_thian@hotmail.com>
2012-04-04 23:39:06 +0800

    Changes to structure of program.
    Tries to grab the URL, still buggy

--- HTMLClient/src/HTMLClient.java
+++ HTMLClient/src/HTMLClient.java
@@ -6,72 +6,153 @@ import java.net.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JFileChooser;
 import javax.swing.JTextField;
 import javax.swing.JLayeredPane;
 import javax.swing.JLabel;
+import javax.swing.UIManager;
+
 import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.JTextPane;
 import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 
 
 public class HTMLClient extends JFrame{
 	private static JTextArea Url;
 	private static JTextArea filePath;
+	private static String websiteAddress;
+	private static File choosenFilePath;
 	
 	public HTMLClient() {
 		getContentPane().setLayout(null);
 		
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		} catch (Exception e) {
+			System.out.println("Unable to load Windows look and feel.");
+		}
 		
 		JLabel lblNewLabel = new JLabel("URL:");
 		lblNewLabel.setFont(new Font("Gill Sans MT", Font.BOLD, 14));
 		lblNewLabel.setBounds(10, 33, 41, 29);
 		getContentPane().add(lblNewLabel);
+			
+		Url = new JTextArea();
+		Url.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		Url.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusLost(FocusEvent e) {			
+					websiteAddress = Url.getText();				
+			}
+		});
+		Url.setWrapStyleWord(true);
+		Url.setLineWrap(true);
+		Url.setBounds(90, 36, 484, 22);
+		getContentPane().add(Url);		
 		
 		JLabel lblFilePath = new JLabel("File Path:");
 		lblFilePath.setFont(new Font("Gill Sans MT", Font.BOLD, 14));
 		lblFilePath.setBounds(10, 95, 68, 29);
 		getContentPane().add(lblFilePath);
 		
-		JButton btnNewButton = new JButton("Get");
-		btnNewButton.setFont(new Font("Cooper Std Black", Font.PLAIN, 18));
-		btnNewButton.setBounds(241, 165, 102, 38);
-		getContentPane().add(btnNewButton);
-			
-		Url = new JTextArea();
-		Url.setWrapStyleWord(true);
-		Url.setLineWrap(true);
-		Url.setBounds(90, 36, 484, 22);
-		getContentPane().add(Url);		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+		scrollPane.setBounds(90, 98, 384, 45);
+		getContentPane().add(scrollPane);
 		
 		filePath = new JTextArea();
-		filePath.setBounds(90, 98, 484, 22);
-		getContentPane().add(filePath);
+		filePath.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		scrollPane.setViewportView(filePath);
+		filePath.setEditable(false);
+		
+		JButton btnBrowse = new JButton("browse");
+		btnBrowse.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnBrowse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JFileChooser chooser = new JFileChooser(".");
+				chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+				chooser.showOpenDialog(null);	
+				choosenFilePath = chooser.getSelectedFile();
+				filePath.setText(choosenFilePath.getPath());
+			}
+		});
+		btnBrowse.setBounds(485, 99, 89, 23);
+		getContentPane().add(btnBrowse);
+		
+		JButton GetButton = new JButton("Get");
+		GetButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {			
+					HTMLGrabber();				
+			}
+		});
+		GetButton.setFont(new Font("Cooper Std Black", Font.PLAIN, 18));
+		GetButton.setBounds(241, 165, 102, 38);
+		getContentPane().add(GetButton);
 	}
 	
-    public static void main (String args[]) throws Exception 
-	{
-		// throws Exception here because don't want to deal
-		// with errors in the rest of the code for simplicity.
-		
+	public static void HTMLGrabber() {
+		String userInputAddress = websiteAddress;
+		String host;
+		String serverPathAddress;
+		String htmlFileName;
+		String localFile = null;
+		String localFilePath = choosenFilePath.getPath();
+
 		Socket s;
 		FileOutputStream myFile;
-		String websiteAddress;
-		String HTMLFile;
-		String localFile = filePath.getText();
+		
+		
+		if (userInputAddress.startsWith("http://")) {
+			userInputAddress = userInputAddress.substring(7);
+		}
+		
+		int indexOfPath = userInputAddress.indexOf("/");
+		serverPathAddress = userInputAddress.substring(indexOfPath);
+		host = userInputAddress.substring(0,indexOfPath);
+		int lastIndex = userInputAddress.lastIndexOf("/");
+		htmlFileName = userInputAddress.substring(lastIndex);
+		localFile = htmlFileName;
+		
+		if (htmlFileName.length() == 0) {
+			htmlFileName = serverPathAddress;
+			localFile = "index.html";
+		}
 		
 		try {
-			websiteAddress = Url.getText();
 			
-			s = new Socket(websiteAddress,80);
+			
+			s = new Socket(host,80);
+			
+			
 			
 			BufferedReader inFromServer = new BufferedReader (new InputStreamReader(s.getInputStream()));
 			
-			OutputStreamWriter outFromClient = new OutputStreamWriter(s.getOutputStream()); 
+			PrintWriter out = new PrintWriter(s.getOutputStream());
+			out.println("GET " + serverPathAddress + " HTTP/1.0");
+			out.println("Host: " + host);
+			out.println();
 			
-			outFromClient.write("GET " + websiteAddress + " HTTP/1.0\r\n");
+			
+			
+			/*OutputStreamWriter outFromClient = new OutputStreamWriter(s.getOutputStream()); 
+			System.out.println("GET " + serverPathAddress + " HTTP/1.0 \n");
+			System.out.println("Host: "+ host);
+			System.out.println("");
+			
+			outFromClient.write("GET " + serverPathAddress + " HTTP/1.0 \n");
+			outFromClient.write("Host: "+ host);
+			outFromClient.write("");
 			outFromClient.flush();
-			
+			*/
 			localFile = localFile.substring(localFile.lastIndexOf('/') +1);
 			
 			BufferedWriter writeToLocalFile = new BufferedWriter(new FileWriter(localFile));
@@ -98,10 +179,18 @@ public class HTMLClient extends JFrame{
 		} catch (IOException e) {
 			System.out.println("Error getting page " + e);
 		}
-		
-		
-		
-		
+	}
+	
+	
+	
+    public static void main (String args[]) throws Exception 
+	{
+		// throws Exception here because don't want to deal
+		// with errors in the rest of the code for simplicity.
+		HTMLClient myHTML = new HTMLClient();
+		myHTML.setSize(600, 330);
+        myHTML.setMinimumSize(new Dimension(600, 330));
+    	myHTML.setVisible(true);
 		
 		
 		
